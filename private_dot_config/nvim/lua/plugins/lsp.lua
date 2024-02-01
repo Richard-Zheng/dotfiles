b97fec324114fc9371fbe829143fb7ceffc5979e
Richard-Zheng <yuqiu36@live.com>
2024-02-01 14:40:48 +0800

    neovim: autocompletion

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,20 +1,81 @@
 return {
   {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "L3MON4D3/LuaSnip", lazy = true },
+      { "saadparwaiz1/cmp_luasnip", lazy = true },
+    },
+    config = function()
+      -- Set up nvim-cmp.
+      local cmp = require'cmp'
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
-    dependencies = { { "folke/neodev.nvim", lazy = true } },
+    dependencies = {
+      { "folke/neodev.nvim", lazy = true },
+      { 'hrsh7th/cmp-nvim-lsp', lazy = true },
+    },
     config = function()
       -- IMPORTANT: make sure to setup neodev BEFORE lspconfig
       require("neodev").setup({
         -- add any options here, or leave empty to use the default settings
       })
+      -- Add additional capabilities supported by nvim-cmp
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
       -- pacman -S lua-language-server
-      lspconfig.lua_ls.setup({})
-      lspconfig.denols.setup({ single_file_support = true })
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.denols.setup({
+        capabilities = capabilities,
+        single_file_support = true
+      })
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code action" })
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+          vim.opt_local.completeopt:remove('preview')
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        end,
+      })
     end,
   }
 }
