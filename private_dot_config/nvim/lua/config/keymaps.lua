4b73c0a3eb350220e6b2cabfc3c89f43933b57d3
Richard-Zheng <yuqiu36@live.com>
2026-05-20 17:08:32 +0800

    add nvim C-b toggle explorer

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,6 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "J", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "K", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+
+-- Toggle Neo-tree with Ctrl+b
+vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<cr>", { desc = "Toggle Explorer" })
